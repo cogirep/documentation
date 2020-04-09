57cdb450fed3573d1380cf1cbaa327ef92319803
Ludovic Wyffels <ludovic.wyffels-isc.france@fr.ibm.com>
2020-04-09 16:12:30 +0200

    doc: sql

--- 01.byd-DataFactory/7.DataBase/sql/v2-organisation-groups/delta.up.sql
+++ 01.byd-DataFactory/7.DataBase/sql/v2-organisation-groups/delta.up.sql
@@ -1,12 +1,31 @@
 BEGIN;
+
+--
+-- Organisation table
+---
+CREATE TABLE beyond.organisation
+(
+    pk_id bigserial NOT NULL ,
+    label character varying,
+    description character varying,
+    CONSTRAINT pk_organisation PRIMARY KEY (pk_id)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE beyond.organisation
+    OWNER to "ibm-cloud-base-user";
+
 --
 -- Groups table
 ---
 CREATE TABLE beyond.groups
 (
-    pk_id bigint NOT NULL DEFAULT nextval('beyond.group_pk_id_seq'::regclass),
-    label character varying COLLATE pg_catalog."default",
-    description character varying COLLATE pg_catalog."default",
+    pk_id bigserial NOT NULL,
+    label character varying,
+    description character varying,
     pk_fk_organisationid bigint,
     CONSTRAINT pk_group PRIMARY KEY (pk_id),
     CONSTRAINT fk_group_organisation FOREIGN KEY (pk_fk_organisationid)
@@ -23,31 +42,13 @@ ALTER TABLE beyond.groups
     OWNER to "ibm-cloud-base-user";
 
 --
--- Organisation table
----
-CREATE TABLE beyond.organisation
-(
-    pk_id bigint NOT NULL DEFAULT nextval('beyond.organisation_pk_id_seq'::regclass),
-    label character varying COLLATE pg_catalog."default",
-    description character varying COLLATE pg_catalog."default",
-    CONSTRAINT pk_organisation PRIMARY KEY (pk_id)
-)
-WITH (
-    OIDS = FALSE
-)
-TABLESPACE pg_default;
-
-ALTER TABLE beyond.organisation
-    OWNER to "ibm-cloud-base-user";
-
---
 -- groups_account table
 ---
 CREATE TABLE beyond.groups_account
 (
-    pk_fk_accountid integer NOT NULL,
-    pk_fk_groupid integer NOT NULL,
-    status character varying COLLATE pg_catalog."default" NOT NULL,
+    pk_fk_accountid bigint NOT NULL,
+    pk_fk_groupid bigint NOT NULL,
+    status character varying,
     CONSTRAINT pk_group_account PRIMARY KEY (pk_fk_accountid, pk_fk_groupid),
     CONSTRAINT fk_account_group_group FOREIGN KEY (pk_fk_groupid)
         REFERENCES beyond.groups (pk_id) MATCH SIMPLE
