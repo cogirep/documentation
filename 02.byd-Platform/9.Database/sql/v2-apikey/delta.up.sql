2d660a5cdedefa9fdb5fe0be0353d1db9ec01c32
Ludovic Wyffels <ludovic.wyffels-isc.france@fr.ibm.com>
2020-06-17 08:42:51 +0200

    doc: update generate a UUID by default

--- 02.byd-Platform/9.Database/sql/v2-apikey/delta.up.sql
+++ 02.byd-Platform/9.Database/sql/v2-apikey/delta.up.sql
@@ -1,22 +1,22 @@
 BEGIN;
 
 CREATE  TABLE beyond_core.apikey ( 
-	pk_id                bigserial  NOT NULL ,
-	fk_product           bigint  NOT NULL ,
-	code                 text  NOT NULL ,
-	apikey_name          text  NOT NULL ,
-	apikey_value         text  NOT NULL ,
-	"role"               text  NOT NULL ,
-	status               bigint  NOT NULL ,
-	created_at           timestamptz DEFAULT current_timestamp NOT NULL ,
-	created_by           bigint  NOT NULL ,
-	updated_at           timestamptz DEFAULT current_timestamp NOT NULL ,
-	updated_by           bigint  NOT NULL ,
+	pk_id           bigserial  NOT NULL,
+	fk_product      bigint  NOT NULL,
+	code            text  NOT NULL,
+	apikey_name     text  NOT NULL,
+	apikey_value    text  NOT NULL DEFAULT md5(random()::text || clock_timestamp()::text)::uuid,
+	"role"          text  NOT NULL CHECK ("role" IN ('CLIENT', 'PROVIDER')),
+	status          bigint  NOT NULL,
+	created_at      timestamptz DEFAULT current_timestamp NOT NULL,
+	created_by      bigint  NOT NULL,
+	updated_at      timestamptz DEFAULT current_timestamp NOT NULL,
+	updated_by      bigint  NOT NULL,
 	CONSTRAINT pk_apikey_pk_id PRIMARY KEY ( pk_id ),
-	CONSTRAINT fk_apikey_product FOREIGN KEY ( fk_product ) REFERENCES core.product( pk_id )  ,
-	CONSTRAINT fk_apikey_status FOREIGN KEY ( status ) REFERENCES core.status( pk_id )  ,
-	CONSTRAINT fk_apikey_account FOREIGN KEY ( created_by ) REFERENCES core.account( pk_id )  ,
-	CONSTRAINT fk_apikey_account_0 FOREIGN KEY ( updated_by ) REFERENCES core.account( pk_id )  
- );
+	CONSTRAINT fk_apikey_product FOREIGN KEY ( fk_product ) REFERENCES beyond_core.product( pk_id ),
+	CONSTRAINT fk_apikey_status FOREIGN KEY ( status ) REFERENCES beyond_core.status( pk_id ),
+	CONSTRAINT fk_apikey_account_createdby FOREIGN KEY ( created_by ) REFERENCES beyond_core.account( pk_id ),
+	CONSTRAINT fk_apikey_account_updatedby FOREIGN KEY ( updated_by ) REFERENCES beyond_core.account( pk_id )
+);
 
 END;
