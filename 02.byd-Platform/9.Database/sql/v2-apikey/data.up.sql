2fccb230072ccb24f9e73c6f3b18ecbb8aac4d43
Ludovic Wyffels <ludovic.wyffels-isc.france@fr.ibm.com>
2020-06-17 11:25:14 +0200

    doc: add sql script for insert data

---
+++ 02.byd-Platform/9.Database/sql/v2-apikey/data.up.sql
@@ -0,0 +1,21 @@
+BEGIN;
+
+WITH vars AS (
+  SELECT 
+    status.pk_id as statusid,
+    account.pk_id as accountid
+  FROM 
+    beyond_core.account as account,
+    beyond_core.status as status 
+  WHERE account.email = 'ludovic.wyffels-isc.france@fr.ibm.com'
+  AND status.code = 'ACTIF'
+)
+INSERT INTO beyond_core.apikey
+  (fk_product, code, apikey_name, "role", status, created_by, updated_by)
+VALUES 
+  ((SELECT pk_id FROM beyond_core.product WHERE label = 'BEYOND MANAGER'), 'BEYOND_MANAGER', 'Beyond Manager API Key', 'PROVIDER', ((SELECT statusid FROM vars)), (SELECT accountid FROM vars), (SELECT accountid FROM vars)),
+  ((SELECT pk_id FROM beyond_core.product WHERE label = 'REALITY MANAGER'), 'REALITY_MANAGER', 'Reality Manager API Key', 'PROVIDER', ((SELECT statusid FROM vars)), (SELECT accountid FROM vars), (SELECT accountid FROM vars));
+
+-- SELECT * FROM beyond_core.apikey;
+
+END;
